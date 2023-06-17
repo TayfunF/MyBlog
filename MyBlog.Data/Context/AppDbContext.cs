88792d429eda685faf83636dcfaacecf0208056e
TayfunF <firtinatayfun08@gmail.com>
2023-06-17 10:10:46 +0300

    AppDbContext ve EFCore paketlerim eklendi

---
+++ MyBlog.Data/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+    }
+}
