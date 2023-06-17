2c6441cbfb92f103efc32518a178ede9bb84dcec
TayfunF <firtinatayfun08@gmail.com>
2023-06-17 21:20:45 +0300

    ArticleDto ve Category Dto oluşturuldu. Mapper sınıfıma eklemeleri yapıldı.

---
+++ MyBlog.Service/AutoMapper/ArticleProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MyBlog.Entity.DTOs.Articles;
+using MyBlog.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Service.AutoMapper
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<Article,ArticleDto>().ReverseMap();
+        }
+    }
+}
---
+++ MyBlog.Service/AutoMapper/CategoryProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MyBlog.Entity.DTOs.Categories;
+using MyBlog.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Service.AutoMapper
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
