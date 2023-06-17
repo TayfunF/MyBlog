b4b5b0f7d20e3863885f7234d463c25e78c8e8bd
TayfunF <firtinatayfun08@gmail.com>
2023-06-17 10:07:41 +0300

    Entitileri oluşturdum

---
+++ MyBlog.Entity/Entities/Image.cs
@@ -0,0 +1,16 @@
+﻿using MyBlog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Entity.Entities
+{
+    public class Image : EntityBase
+    {
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
