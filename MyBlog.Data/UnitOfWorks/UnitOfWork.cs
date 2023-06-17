a4ba85ef934d46c152ba5d3c78d67d2377792f8b
TayfunF <firtinatayfun08@gmail.com>
2023-06-17 10:24:25 +0300

    UnitOfwork ekledim

---
+++ MyBlog.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using MyBlog.Data.Context;
+using MyBlog.Data.Repositories.Abstracts;
+using MyBlog.Data.Repositories.Concerets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        IGenericRepository<T> IUnitOfWork.GetRepository<T>()
+        {
+            return new GenericRepository<T>(_context);
+        }
+    }
+}
