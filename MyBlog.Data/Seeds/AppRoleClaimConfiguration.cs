bf821d9bf68d172b4c8ef703db52ae47e61bb460
TayfunF <firtinatayfun08@gmail.com>
2023-06-17 12:58:36 +0300

    Seed methodlarım oluşturuldu.

---
+++ MyBlog.Data/Seeds/AppRoleClaimConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using MyBlog.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Data.Seeds
+{
+    public class AppRoleClaimConfiguration : IEntityTypeConfiguration<AppRoleClaim>
+    {
+        public void Configure(EntityTypeBuilder<AppRoleClaim> builder)
+        {
+            // Primary key
+            builder.HasKey(rc => rc.Id);
+
+            // Maps to the AspNetRoleClaims table
+            builder.ToTable("AspNetRoleClaims");
+        }
+    }
+}
