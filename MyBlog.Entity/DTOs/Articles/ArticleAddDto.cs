bed9307705f114a3963feaadb63a4cf6e708a477
TayfunF <firtinatayfun08@gmail.com>
2023-06-29 13:20:49 +0300

    Resim yükleme işlemi Controllerda uygulandı. Var olan resmi güncelleme yapılacak.

--- MyBlog.Entity/DTOs/Articles/ArticleAddDto.cs
+++ MyBlog.Entity/DTOs/Articles/ArticleAddDto.cs
@@ -13,11 +13,7 @@ namespace MyBlog.Entity.DTOs.Articles
         public string Title { get; set; }
         public string Content { get; set; }
         public Guid CategoryId { get; set; }
+        public IFormFile Photo { get; set; }
         public IList<CategoryDto> Categories { get; set; }
-        public Guid AppUserId { get; set; }
-        public AppUser AppUser { get; set; }
-        public Guid ImageId { get; set; }
-        public Image Image { get; set; }
-
     }
 }
